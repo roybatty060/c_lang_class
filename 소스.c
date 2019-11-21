df12a4504a7bcc22e72abc2193dbe587cdda2190
RoyBatty <roybatty0601@gmail.com>
2019-11-21 14:30:27 +0900

    2019-11-21

--- 소스.c
+++ 소스.c
@@ -1,67 +1,118 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int last_index = 0, i = 1;
+static char daytab[2][13] = {
+	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31} //
+};
+static int day[2] = {365, 366};
+static char *days[7] = {"mon", "tue", "wed", "thu", "fri", "sat", "sun"};
+int total = 0;
+int pmonth, pday, plus_year = 0;
 
-int function(int list[], int arg, char *args[])
+int day_of_year(int year, int month, int day)
 {
-	int value;
-	while (i == arg || args[i][0] < '0' || args[i][0] == 'x')
+	int i, leap;
+	leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	for (i = 1; i < month; i++)
+		day += daytab[leap][i];
+	return day;
+}
+
+void month_day(int year, int month, int yearday, int *pmonth, int *pday)
+{
+	int i, leap;
+	leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	for (i = month; yearday > daytab[leap][i]; i++)
+		yearday -= daytab[leap][i];
+	*pmonth = i;
+	*pday = yearday;
+}
+
+int else_day(int year, int month, int day)
+{
+	int i, leap, sum = 0;
+	leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	sum += daytab[leap][month] - day;
+	for (i = month + 1; i <= 12; i++)
+		sum += daytab[leap][i];
+	return sum;
+}
+
+int between(int year1, int month1, int day1, int year2, int month2, int day2)
+{
+	int leap1, leap2;
+	int error1, error2;
+	int i, sample_leap;
+	leap1 = year1 % 4 == 0 && year1 % 100 != 0 || year1 % 400 == 0;
+	leap2 = year2 % 4 == 0 && year2 % 100 != 0 || year2 % 400 == 0;
+
+	error1 = year1 < 1 || year1 > 3000 || day1 < 1 || day1 > daytab[leap1][month1] || month1 < 1 || month1 > 12;
+	error2 = year2 < 1 || year2 > 3000 || day2 < 1 || day2 > daytab[leap2][month2] || month2 < 1 || month2 > 12;
+	if (error1 || error2)
+		return -1;
+	total += else_day(year1, month1, day1);
+	for (i = year1 + 1; i < year2; i++)
 	{
-		if (i == arg)
-			return 1;
-		switch (args[i][0])
-		{
-		case '+':
-			value = list[last_index - 1] + list[last_index];
-			break;
-		case '-':
-			value = list[last_index - 1] - list[last_index];
-			break;
-		case 'x':
-			value = list[last_index - 1] * list[last_index];
-			break;
-		case '%':
-			if (list[last_index] == 0)
-				return -1;
-			value = list[last_index - 1] / list[last_index];
-			break;
-		}
-		list[--last_index] = value;
-		i++;
+		sample_leap = i % 4 == 0 && i % 100 != 0 || i % 400 == 0;
+		total += day[sample_leap];
 	}
-	last_index++;
+	total += day_of_year(year2, month2, day2);
 	return 0;
+}
+
+void one_sample(int year, int month, int day)
+{
+	int num = between(1, 1, 1, year, month, day);
+	if (num == -1) {
+		printf("error");
+		return 0;
+	}
+	else
+		printf("%s", days[total % 7]);
+}
+
+void after(int year, int month, int day, int days)
+{
+	int left = days - else_day(year, month, day);
+	if (left>0) {
+		month_day(year + 1, 1, left % 365, &pmonth, &pday);
+		printf("%d.%d.%d", year + left / 365 + 1, pmonth, pday);
+	}
+	else {
+		month_day(year, month, days, &pmonth, &pday);
+		printf("%d.%d.%d", year, pmonth, pday+day);
+	}
+
 }
 
 int main(int arg, char *args[])
 {
-	int list[100];
-	int num;
-
-	if (arg - 1 < 10)
+	int leap1, year1, month1, day1;
+	int leap2, year2, month2, day2;
+	int error1, error2;
+	int i, k1, k2, num;
+	k1 = sscanf(args[1], "%d.%d.%d", &year1, &month1, &day1);
+	if (k1 < 3)
 	{
 		printf("error");
 		return 0;
 	}
-	while (1)
+	if (arg == 2)
+		one_sample(year1, month1, day1);
+	else
 	{
-		if (args[i][0] < '0' || args[i][0] == 'x')
-		{
-			last_index--;
-			num = function(list, arg, args);
+		k2 = sscanf(args[2], "%d.%d.%d", &year2, &month2, &day2);
+		if (k2 == 1)
+			after(year1, month1, day1, year2);
+		else if (k2 == 3) {
+			num = between(year1, month1, day1, year2, month2, day2);
 			if (num == -1)
-			{
 				printf("error");
-				return 0;
-			}
-			else if (num == 1)
-			{
-				printf("%d", list[0]);
-				return 0;
-			}
 			else
-				continue;
+				printf("%d", total);
 		}
-		list[last_index++] = args[i++][0] - '0';
+		else
+			printf("error");
 	}
 }
